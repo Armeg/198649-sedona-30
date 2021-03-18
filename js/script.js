b3c825e254a2036976fb53c9af064525633faeb0
Armeg <69594602+Armeg@users.noreply.github.com>
2021-03-18 10:30:41 +0200

    interactive elements of the main page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 
-  const onclick = document.querySelector(".search-hotels-form");
+  const form_managment = document.querySelector(".search-hotels-form");
   let form = document.querySelector('.form-search-hotels');
+  const date_arrive = form.querySelector('[name=hotel-date-arrive]');
+  const date_leave = form.querySelector('[name=hotel-date-leave]')
 
-  onclick.addEventListener("click", function (evt) {
+
+  form_managment.addEventListener("click", function (evt) {
     evt.preventDefault();
-    form.classList.toggle('search-hotels-form-disabled')
+    form.classList.toggle('search-hotels-form-disabled');
+    form.classList.toggle('form-search-hotels');
 });
 
+form.addEventListener('submit', function (evt) {
+if (!date_arrive.value || !date_leave.value) {
+  evt.preventDefault();
+  form.classList.add('modal-error');
+  console.log('write dates');
+} else {
+  form.classList.remove('modal-error');
+}
+});
